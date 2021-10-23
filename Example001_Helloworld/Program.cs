3ffe9ef033006a7e9faae1c4bfc1bc3a54addb7a
Dzheb <Dzheb2009@gmail.com>
2021-10-23 23:10:58 +0300

    First commit

---
+++ Example001_Helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example001_Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
