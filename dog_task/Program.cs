53004e995a1702c58a4fe63674895c9797fbc954
Dzheb <Dzheb2009@gmail.com>
2021-10-26 21:01:04 +0300

    arraay intro

---
+++ dog_task/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int minDistance = 10;
+
+while (distance > minDistance)
+{
+     
+}
+
+Console.WriteLine($"Собака пробежит {count} раз");
+
+
+
