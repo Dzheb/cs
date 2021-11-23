678b18464b3829354d9e0bba934a9dad9b824221
Dzheb <Dzheb2009@gmail.com>
2021-11-23 21:19:04 +0300

    Добавлена рекурсия

--- practicum1/Program.cs
+++ practicum1/Program.cs
@@ -311,6 +311,256 @@ Console.Write("Массив элементов со знакочередован
 PrintArray(arrayB32);
 Console.WriteLine();
 
+// Задача 1
+// Подсчитать количество натуральных чисел, не превосходящих заданного числа n, которые
+
+// делятся на k, но на l
+// делятся хотябы на k или на l
+// не делятся на (k + l)
+
+// задание n,k,l
+int n = 20;
+int k = 2;
+int l = 3;
+// количество чисел которые делятся на k, но не на l
+int i = 1;
+int count = 0;
+while (i < n)
+{
+  if (i % k == 0 && i % l != 0)
+  {
+    count++;
+    Console.WriteLine($"Число, которое делятся на {k} и не на {l}: {i}");
+
+  }
+  i++;
+}
+Console.WriteLine($"Кол-во чисел, которые делятся на {k} и не на {l}: { count}");
+
+// делятся хотябы на k или на l
+i = 1;
+count = 0;
+while (i < n)
+{
+  if (i % k == 0 || i % l == 0)
+  {
+    count++;
+    Console.WriteLine($"Число, которое делятся на {k} или на {l}: {i}");
+
+  }
+  i++;
+}
+Console.WriteLine($"Кол-во чисел, которые делятся на {k} или на {l}: { count}");
+// не делятся на (k + l)
+i = 1;
+int m = k + l;
+count = 0;
+while (i < n)
+{
+  if (i % m != 0)
+  {
+    count++;
+    Console.WriteLine($"Число, которое не делятся на ({k} + {l}): {i}");
+
+  }
+  i++;
+}
+Console.WriteLine($"Кол-во чисел, которые не делятся на {k} + {l}: { count}");
+
+//
+// Задача 2
+//
+// Задать положительное вещественное число m. Cоставить целое число n из цифр
+//
+// десятков и сотых m
+// единиц и сотых m
+// сотен и десятых m
+
+double m2 = 75874.4534;
+System.Console.WriteLine("Задача 2 практикума :");
+Console.WriteLine($"целое число n из десятков и сотых m : {ExtDecSot(m2)}");
+Console.WriteLine($"целое число n из единиц и сотых m m : {ExtEdSot(m2)}");
+Console.WriteLine($"целое число n из сотен и десятых m : {ExtSotDec(m2)}");
+
+
+
+// десятков и сотых m
+int ExtDecSot(double indouble)
+{
+  return ((int)m2 / 10 % 10) * 10 + (int)((m2 - (int)m2) * 100) % 10;
+}
+// единиц и сотых m
+int ExtEdSot(double indouble)
+{
+  return ((int)m2 % 10) * 10 + (int)((m2 - (int)m2) * 100) % 10;
+}
+// сотен и десятых m
+int ExtSotDec(double indouble)
+{
+  return ((int)m2 / 100 % 10) * 10 + (int)((m2 - (int)m2) * 10) % 10;
+}
+
+
+// Задача 3
+// Написать программу, вычисляющую значение периодической функции f(x), в произвольной точке x, если на промежутке [a;b], который составляет один её период, значения функции вычисляются по формуле
+
+// [-3; 3], f(x) = |x| - 1
+// [-10; -2], f(x) = 1/x + 0.6
+// [-4; 3], f(x) = Cos(|π*x|)
+
+int x;
+int y;
+//Console.Clear();
+// 1.
+for (x = -3; x < 3; x++)
+{
+  y = Math.Abs(x) - 1;
+
+  Console.WriteLine($"x = {x}  y = {y}");
+}
+// 2.
+double ydouble = 0;
+for (x = -10; x < -2; x++)
+{
+  if (x != 0)
+  {
+    ydouble = 1 / x + 0.6;
+  }
+
+  Console.WriteLine($"x = {x}  y = {ydouble}");
+}
+
+// 3.  [-4; 3], f(x) = Cos(|π*x|)
+ydouble = 0;
+for (x = -4; x <= 3; x++)
+{
+
+  ydouble = Math.Cos((Math.PI * x));
+
+  Console.WriteLine($"x = {x}  y = {ydouble}");
+}
+
+// Задача 4
+// Даны координаты концов двух отрезков [a; b], [c; d]. Верно ли, что
+
+// отрезки не пересекаются
+// отрезки пересекаются строго в одной точке
+// один отрезок вложен в другой
+// [a,b]
+int xa4 = 1;
+int ya4 = 5;
+int xb4 = 7;
+int yb4 = 20;
+//
+// [c,d]
+int xc4 = 2;
+int yc4 = 8;
+int xd4 = 4;
+int yd4 = 9;
+
+int[,] ArrInt1 = FillInter(xa4, ya4, xb4, yb4);
+int[,] ArrInt2 = FillInter(xc4, yc4, xd4, yd4);
+
+Console.WriteLine("Задача 4 вектор линии 1");
+if (ArrInt1.Length != 0) PrintArr2Dim(ArrInt1);
+else Console.WriteLine("По заданным координатам невозможно построить отрезок");
+
+Console.WriteLine("Задача 4 вектор линии 2");
+if (ArrInt2.Length != 0) PrintArr2Dim(ArrInt2);
+else Console.WriteLine("По заданным координатам невозможно построить отрезок");
+// Ответ задачи
+Console.WriteLine(IsRel(ArrInt1, ArrInt2));
+
+
+string IsRel(int[,] curarr, int[,] targetarr)
+{
+  string res = string.Empty;
+  int count = 0;
+  int rows = curarr.GetUpperBound(0) + 1;    // количество строк
+  int columns = curarr.Length / rows;  // количество столбцов
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < columns - 1; j++)
+    {
+      if (IsinTarget(curarr[i, j], curarr[i, j + 1], targetarr))
+      {
+        count = count + 1;
+      }
+    }
+  }
+  if (count == 0) res = "нет точек пересечения отрезков";
+  if (count == 1) res = "отрезки пересекаются в одной точке";
+  if (count > 1) res = "один отрезок принадлежит другому";
+
+  return res;
+}
+
+bool IsinTarget(int x4, int y4, int[,] checkarr)
+{
+  bool res = false;
+  int rows = checkarr.GetUpperBound(0) + 1;    // количество строк
+  int columns = checkarr.Length / rows;  // количество столбцов
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < columns - 1; j++)
+    {
+      if (checkarr[i, j] == x4 && checkarr[i, j + 1] == y4)
+      {
+        res = true;
+      }
+    }
+  }
+  return res;
+}
+
+// получение всех точек отрезка
+int[,] FillInter(int x1, int y1, int x2, int y2)
+{
+  int[,] res = new int[x2 - x1 + 1, 2];
+  int rows = res.GetUpperBound(0) + 1;    // количество строк
+  int columns = res.Length / rows;  // количество столбцов
+  int curpoint = x1;// сохранение значения х первой точки
+  // если координаты конечной точки по оси х меньше меняем местами значения х1 и х2
+  if (x1 > x2)
+  {
+    x1 = x2;
+    x2 = curpoint;
+  }
+  curpoint = x1;// сохранение значения х первой точки ещё раз
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < columns; j++)
+    {
+      if (j % 2 == 0) res[i, j] = curpoint;
+      else res[i, j] = (int)((double)(curpoint - xa4) / (xb4 - xa4) * (double)(yb4 - ya4) + ya4);
+    }
+    curpoint++;
+  }
+  return res;
+}
+
+void PrintArr2Dim(int[,] arr)
+{
+  int rows = arr.GetUpperBound(0) + 1;    // количество строк
+  int columns = arr.Length / rows;        // количество столбцов
+
+  for (int i = 0; i < rows; i++)
+  {
+    Console.Write($"Точка {i + 1}");
+    for (int j = 0; j < columns; j++)
+    {
+      if (j % 2 == 0) Console.Write($"  x: {arr[i, j]}");
+      else Console.Write($"  y: {arr[i, j]}");
+
+    }
+    Console.WriteLine("");
+  }
+}
+// конец Задачи 4
+
 // Задача 5
 // Проверить, что натуральное число m
 // является счастливым пятизначным
@@ -421,6 +671,8 @@ bool IsEqual(int m5)
   if (NumDec(m5, 1) == NumDec(m5, 2) && NumDec(m5, 1) == NumDec(m5, 3) && NumDec(m5, 1) == NumDec(m5, 4)) return true;
   else return false;
 }
+// конец задачи 5
+
 // Задача 6
 // Выяснить является ли вещественное m
 // целой степенью числа 3
@@ -495,4 +747,360 @@ bool Sin45(double anglesin)
   }
   return res;
 }
+// конец задачи 6
+
+
+
+
+// Задача 9 - решение в общем виде
+// Проверить, что точка с координатами (x, y) принадлежит фигуре начало координат находится в верхнем левом углу
+// 9.1
+// размеры viewport 4*4
+int maxcol = 4;
+int maxrow = 4;
+// принадлежит ли точка с координатами фигуре 1
+// задание координат фигуры x и y двумя массивами
+// фигура П
+// int[] arrxm = { 0, 1, 2, 3, 4, 0, 4, 0, 4, 0, 4, 0, 4 };
+// int[] arrym = { 0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4 };
+// фигура Q
+// int[] arrxm = { 0, 1, 2, 3, 4, 0, 4, 0, 1, 2, 3, 4, 4 };
+// int[] arrym = { 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 2, 3 };
+// фигура U
+int[] arrxm = { 0, 4, 0, 4, 0, 4, 0, 1, 2, 3, 4 };
+int[] arrym = { 0, 0, 1, 1, 2, 2, 3, 3, 3, 3, 3 };
+// задание координат точки
+int xa = 5;
+int ya = 2;
+
+// рисование фигуры
+Writearr(arrxm, arrym, maxrow, maxcol);
+
+// определение принадлежит ли точка данной фигуре
+Console.WriteLine("");
+if (MatchPoint(arrxm, arrym, maxrow, maxcol, xa, ya))
+{
+  Console.WriteLine($"Точка с координатами  x: {xa} и y: {ya} принадлежит этой фигуре");
+}
+else
+{
+  Console.WriteLine($"Точка с координатами  x: {xa} и y: {ya} не принадлежит этой фигуре");
+}
+
+//
+//
+// блок функций
+// функция рисования фигуры
+void Writearr(int[] arrx, int[] arry, int maxrow, int maxcol)
+{
+  i = 0;
+  int j = 0;
+  Console.Clear();
+  int narr = 0;
+
+  for (j = 0; j < maxrow; j++)
+  {
+    for (i = 0; i <= maxcol; i++)
+    {
+      for (narr = 0; narr < arrx.Length; narr++)
+      {
+        if (arrx[narr] == i && arry[narr] == j)
+        {
+          Console.SetCursorPosition(i, j);
+          Console.Write("+");
+          narr++;
+        }
+      }
+    }
+  }
+}
+
+// функция определения лежит ли точка на фигуре
+Boolean MatchPoint(int[] arrx, int[] arry, int maxrow, int maxcol, int x, int y)
+{
+  i = 0;
+  int j = 0;
+  int narr = 0;
+  Boolean match = false;
+
+  for (j = 0; j < maxrow; j++)
+  {
+    for (i = 0; i <= maxcol; i++)
+    {
+      for (narr = 0; narr < arrx.Length; narr++)
+      {
+
+        if (x == i && y == j)
+        {
+          match = true;
+          break;
+        }
+      }
+    }
+  }
+  return match;
+}
+
+// // Задача 9 первоначальное решение
+// // Проверить, что точка с координатами (x, y) принадлежит фигуре начало координат находится в верхнем левом углу
+// // 9.1
+// i = 0;
+// int j = 0;
+// int maxcol = 4;
+// int maxrow = 4;
+// // принадлежит ли точка с координатами фигуре 1
+// // задание координат x и y двумя массивами
+// int[] arrx = { 0, 1, 2, 3, 4, 0, 4, 0, 4, 0, 4, 0, 4 };
+// int[] arry = { 0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4 };
+// int xa = 4;
+// int ya = 1;
+// Boolean match = false;
+
+// Console.Clear();
+// for (j = 0; j < maxrow; j++)
+// {
+//   if (i == 0)
+//   {
+//     for (i = 0; i <= maxcol; i++)
+//     {
+//       Console.SetCursorPosition(i, j);
+//       Console.Write("+");
+//       if (xa == i && ya == j) match = true;
+
+//     }
+//   }
+//   else
+//   {
+//     for (i = 0; i <= maxcol; i++)
+//     {
+//       if (i == 0 || i == maxcol)
+//       {
+//         if (xa == i && ya == j) match = true;
+//         Console.SetCursorPosition(i, j);
+//         Console.Write("+");
+//       }
+
+//     }
+//   }
+
+// }
+// if (match)
+// {
+//   Console.WriteLine($"Точка с координатами  x: {xa} и y: {ya} принадлежит фигуре");
+// }
+// else
+// {
+//   Console.WriteLine($"Точка с координатами  x: {xa} и y: {ya} не принадлежит фигуре");
+// }
+
+// Задача 11
+//Разложите натуральное число n на простые множители и определите
+//
+// сколько раз встречается множитель 2 в произведении
+// наименьший множитель произведения
+// сколько различных множителей в произведении
+// например число 36
+int n11 = 36;
+// Задаём массив простых чисел до 173
+int[] simpleNumber = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173 };
+int index = 0;
+int count11 = 0;
+Boolean minfound = false;
+int minmult = 0;
+int numult = 0;
+
+while (n11 > 1)
+{
+  if (n11 % simpleNumber[index] == 0)
+
+  {
+    // если минимальный множитель ещё не найден присваиваем min первому
+    if (!minfound)
+    {
+      minmult = simpleNumber[index];
+      minfound = true;
+    }
+    // если множитель чётный увеличиваем счётчик
+    if (simpleNumber[index] % 2 == 0)
+    {
+      count11++;
+    }
+    Console.WriteLine(simpleNumber[index]);
+    n11 = n11 / simpleNumber[index];
+    numult++;
+
+  }
+  else
+  {
+    index++;
+  }
+
+}
+
+Console.WriteLine($"Сколько раз встречается множитель 2 в произведении :  {count}");
+Console.Write("Минимальный множитель равен:  ");
+Console.WriteLine(minmult);
+Console.WriteLine($"Число простых множителей :  {numult}");
+
+
+
+// Задача 22
+// Ввести текст, содержащий числовое выражение заданного вида и найти его значение, например
+// 21+34
+// 
+// 33/11
+// 3^4
+
+// функция определения есть ли + в строке
+char IsSign(string? entstr)
+{
+  int length = entstr!.Length;
+  int i = 0;
+  char result = '@';
+  while (i < length)
+  {
+    if (entstr[i] == '+' || entstr[i] == '/' || entstr[i] == '^')
+    {
+      result = entstr[i];
+    }
+    i++;
+  }
+  return result;
+}
+
+// функция определения номера знака в строке
+int IndexOf(string? entstr, char sign)
+{
+  int length = entstr!.Length;
+  int i = 0;
+  int result = -1;
+  while (i < length)
+  {
+    if (entstr[i] == sign)
+    {
+      result = i;
+      break;
+    }
+    i++;
+  }
+  return result;
+}
+
+// функция определения первого элемента выражения
+string FindFirst(string? entstr, int sign)
+{
+  int i = 0;
+  string result = "";
+  while (i < sign)
+  {
+    result = result + entstr![i];
+    i++;
+  }
+  return result;
+}
+
+// функция определения второго элемента выражения
+string FindSecond(string? entstr, int sign)
+{
+  int length = entstr!.Length;
+  int i = sign + 1;
+  string result = "";
+  while (i < length)
+  {
+    result = result + entstr[i];
+    i++;
+  }
+  return result;
+}
+
+// Основной блок
+// 
+// ввод строки
+string? instring = "";
+Console.Write("Введите строку содержащую операцию '+, / или ^'  : ");
+instring = Console.ReadLine();
+// проверка на пустую строку
+if (instring == "")
+{
+  Console.Write("Вы ввели пустую строку");
+}
+else
+{
+  // проверка на знак операции
+  if (IsSign(instring) == '@')
+  {
+    Console.WriteLine("Вы не ввели необходимый знак операции");
+  }
+  else
+
+  {
+    Console.WriteLine(IsSign(instring));
+    // определение операции по знаку
+    char sign = IsSign(instring);
+    int nsign = IndexOf(instring, sign);
+    // проверка на ввод операндов
+    if (FindFirst(instring, nsign) == "" || FindSecond(instring, nsign) == "")
+    {
+      Console.WriteLine("Вы не ввели необходимые операторы/оператор");
+    }
+    else
+    {
+
+      // 
+      // для определения первого и второго операндов воспользуемся немножко готовым кодом (Int32.Parse)
+      int numfirst = Int32.Parse(FindFirst(instring, nsign));
+      int numsecond = Int32.Parse(FindSecond(instring, nsign));
+      //
+      //Вывод результата
+      switch (sign)
+      {
+        case '+':
+          Console.WriteLine(numfirst + numsecond);
+          break;
+        case '/':
+          Console.WriteLine(numfirst / numsecond);
+          break;
+        case '^':
+          Console.WriteLine(Math.Pow(numfirst, numsecond));
+          break;
+        default:
+          Console.WriteLine("Вы не ввели в строке оператор для расчёта");
+          break;
+      }
+    }
+  }
+}
+
+
+// Задача 7
+// Даны четыре логические значения f1, f2, f3, f4. Записать условие того, что
+
+// более трёх значений истинны
+// хотя бы три значения истинны
+// менее трёх значений истинны
+
+bool f1 = true;
+bool f2 = false;
+bool f3 = false;
+bool f4 = true;
+Console.WriteLine("");
+Console.WriteLine("Задача 7");
+if (f1 && f2 && f3 && f4) Console.WriteLine("более трёх значений истинны");
+if ((f1 && f2 && f3) || (f1 && f3 && f4) || (f2 && f3 && f4)) Console.WriteLine("хотя бы три значения истинны");
+if (!((f1 && f2 && f3) || (f1 && f3 && f4) || (f2 && f3 && f4))) Console.WriteLine("менее трёх значений истинны");
+
+// Задача 8
+// Даны пять целых чисел. Выяснить, что
+
+// три из них равны пяти
+// больше трёх из них равны пяти
+// менее трёх из них равны пяти
+
+int num1 = 5;
+int num2 = 5;
+int num3 = 5;
+int num4 = 4;
+int num5 = 4;
+
+if (((num1 == 5 || num2 == 5) && (num1 != num2)) && ((num3 == 5 || num4 == 5) && (num3 != num4)) && (num5 == 5)) Console.WriteLine("!!!");
 
---
+++ recursion/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Ex001Factorial
+{
+  class Program
+  {
+    static int countFR = 0;
+    static int countFI = 0;
+
+    static double FactorialRecursion(int n)
+    {
+      countFR++;
+      return n == 0 ? 1 : n * FactorialRecursion(n - 1);
+    }
+
+    static double FactorialIteration(int n)
+    {
+      double result = 1;
+      countFI++;
+      for (int i = n; i > 1; i--)
+      {
+        countFI++;
+        result *= i;
+      }
+      return result;
+    }
+    static int Fibonachi(int n)
+    {
+      if (n == 0 || n == 1) return n;
+
+      return Fibonachi(n - 1) + Fibonachi(n - 2);
+    }
+    static int FibonachiIt(int n)
+    {
+      int result = 0;
+      int b = 1;
+      int tmp;
+
+      for (int i = 0; i < n; i++)
+      {
+        tmp = result;
+        result = b;
+        b += tmp;
+      }
+
+      return result;
+    }
+
+
+
+    static void Main(string[] args)
+    {
+      int n = 30;
+      double ResFact;
+
+      Console.WriteLine("Время расчёта через рекурсию");
+      DateTime dt = DateTime.Now;
+      ResFact = FactorialRecursion(n);
+      //ResFact = FactorialIteration(n);
+      Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+
+      // Console.WriteLine("Время расчёта через итерацию");
+      // dt = DateTime.Now;
+      // //ResFact = FactorialIteration(n);
+      // ResFact = FactorialRecursion(n);
+      // Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+
+      Console.WriteLine($"n({n}) = {FactorialRecursion(n)} countFR = {countFR}");
+      Console.WriteLine($"n({n}) = {FactorialIteration(n)} countFI = {countFI}");
+      //Console.ReadLine();
+
+      // вычисление чискл Фибоначчи
+      int fib4 = Fibonachi(4);
+      int fib5 = FibonachiIt(5);
+      int fib6 = Fibonachi(6);
+
+      Console.WriteLine($"4 число Фибоначчи = {fib4}");
+      Console.WriteLine($"5 число Фибоначчи = {fib5}");
+      Console.WriteLine($"6 число Фибоначчи = {fib6}");
+
+    }
+  }
+}
+// вывод - для экономии времени надо использовать циклы вместо роекурсии
